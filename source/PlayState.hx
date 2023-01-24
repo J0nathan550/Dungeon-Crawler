03ede7904443c847d8fc4e8250278dfa7984a575
J0nathan550 <ihudokormov@gmail.com>
2023-01-24 02:12:46 +0200

    Hello World - 01

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,15 @@
 package;
 
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var text = new FlxText(10,10,100,"Hello World");
+		text.screenCenter();
+		add(text);
 		super.create();
 	}
 
