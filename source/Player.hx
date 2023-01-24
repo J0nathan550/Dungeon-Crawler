4c9ffc42e5c9689e806691c04fe8e075e717789e
J0nathan550 <ihudokormov@gmail.com>
2023-01-24 02:52:40 +0200

    Added Player Sprites, and animations - 03

--- source/Player.hx
+++ source/Player.hx
@@ -16,7 +16,17 @@ class Player extends FlxSprite
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
-		makeGraphic(16, 16, FlxColor.BLUE);
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(LEFT, false, false);
+		setFacingFlip(RIGHT, true, false);
+		setSize(8, 8);
+		offset.set(4, 8);
+		animation.add("d_idle", [0]);
+		animation.add("lr_idle", [3]);
+		animation.add("u_idle", [6]);
+		animation.add("d_walk", [0, 1, 0, 2], 6);
+		animation.add("lr_walk", [3, 4, 3, 5], 6);
+		animation.add("u_walk", [6, 7, 6, 8], 6);
 		drag.x = drag.y = 800;
 	}
 
@@ -35,10 +45,9 @@ class Player extends FlxSprite
 		{
 			left = right = false;
 		}
-
-		var newAngle:Float = 0;
 		if (up || down || left || right)
 		{
+			var newAngle:Float = 0;
 			if (up)
 			{
 				newAngle = -90;
@@ -50,6 +59,7 @@ class Player extends FlxSprite
 				{
 					newAngle += 45;
 				}
+				facing = UP;
 			}
 			else if (down)
 			{
@@ -62,17 +72,40 @@ class Player extends FlxSprite
 				{
 					newAngle -= 45;
 				}
+				facing = DOWN;
 			}
 			else if (left)
 			{
 				newAngle = 180;
+				facing = LEFT;
 			}
 			else if (right)
 			{
 				newAngle = 0;
+				facing = RIGHT;
 			}
 			velocity.setPolarDegrees(SPEED, newAngle);
 		}
+
+		var action:String = "idle";
+
+		if ((velocity.x != 0 || velocity.y != 0) && touching == NONE)
+		{
+			action = "walk";
+		}
+
+		switch (facing)
+		{
+			case LEFT, RIGHT:
+				animation.play("lr_" + action);
+			case UP:
+				animation.play("u_" + action);
+			case DOWN:
+				animation.play("d_" + action);
+			case _:
+		}
+
+
 	}
 
 	override function update(elapsed:Float)
